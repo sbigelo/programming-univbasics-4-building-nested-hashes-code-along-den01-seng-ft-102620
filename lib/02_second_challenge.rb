96acff70bb0003e008712d527586579cdb0034f7
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-17 21:34:49 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,5 +1,14 @@
-
-
+def second_challenge
+epic_tragedy = {
+  :montague => {
+    :patriarch => {},
+    :matriarch => {},
+    :hero => {},
+    :hero_friends => []
+  },
+  :capulet => {}
+}
+end
 
 
 
